adf75d76aa05d6fca50960cc99f095388e18944c
Dennis Svatunek <39262379+dsvatunek@users.noreply.github.com>
2025-12-29 16:25:32 +0100

    fixed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import starlight from "@astrojs/starlight";
 
-const SITE = "https://<user>.github.io";
-const BASE = "/<repo>";
+const SITE = "https://svatunek-lab.github.io";
+const BASE = "/notes";
 
 export default defineConfig({
   // Used by integrations like `@astrojs/sitemap` to generate absolute URLs.
